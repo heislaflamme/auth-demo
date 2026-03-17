55ea63c68a1f2119d088631771000d64d2af1b6e
Chidubem <emekaogbuchidubem@gmail.com>
2026-03-17 19:47:15 +0100

    ...

--- src/lib/auth.server.ts
+++ src/lib/auth.server.ts
@@ -7,6 +7,7 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg"
   }),
+  baseURL: process.env.BETTER_AUTH_URL!,
   emailAndPassword: {
     enabled: true,
   },
